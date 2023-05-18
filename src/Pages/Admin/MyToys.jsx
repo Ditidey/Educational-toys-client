7336fd55ac6cf78473065c22706fe4211c63c9e6
Ditidey <ditidey2017@gmail.com>
2023-05-18 12:35:39 +0100

    my toys query link added

--- src/Pages/Admin/MyToys.jsx
+++ src/Pages/Admin/MyToys.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { contextProvider } from '../../AuthProvider';
 
 const MyToys = () => {
+    const [toys, setToys] = useState([]);
+    const {user} = useContext(contextProvider);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allToys?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setToys(data)
+            })
+    }, [user])
     return (
         <div>
             
