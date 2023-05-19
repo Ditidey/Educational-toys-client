30359baa83534a9352a2428f36d8f7aa8ced5227
Ditidey <ditidey2017@gmail.com>
2023-05-19 15:21:33 +0100

    deleted edited

--- src/Pages/Admin/MyToys.jsx
+++ src/Pages/Admin/MyToys.jsx
@@ -22,23 +22,35 @@ const MyToys = () => {
     }, [user, value])
 
     const handleDelete = id =>{
-        fetch(`https://educational-toys-server.vercel.app/allToys/${id}`, {
-            method: 'DELETE'
-        })
-        .then(res =>res.json())
-        .then(data =>{
-            console.log(data)
-            if(data.deletedCount > 0){
-                Swal.fire({
-                    title: 'Delete!',
-                    text: 'Sorry! You have deleted one toy.',
-                    icon: 'success',
-                    confirmButtonText: 'Cool'
-                  })
-                  const remaining = toys.filter(toy => toy._id !== id);
-                  setToys(remaining);
-            }
-        })
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            fetch(`https://educational-toys-server.vercel.app/allToys/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res =>res.json())
+            .then(data =>{
+                console.log(data)
+                if(data.deletedCount > 0){
+                    Swal.fire({
+                        title: 'Delete!',
+                        text: 'Sorry! You have deleted one toy.',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                      })
+                      const remaining = toys.filter(toy => toy._id !== id);
+                      setToys(remaining);
+                }
+            })
+          })
+
+        
     }
     return (
         <div className='mb-10 bg-green-700 rounded-xl p-10'>
--- src/Pages/AllToys.jsx
+++ src/Pages/AllToys.jsx
@@ -25,20 +25,20 @@ const AllToys = () => {
         setNameToy(searchValue);
     }
     return (
-        <div className='my-5  bg-red-900 rounded-xl p-10 py-16'>
+        <div className='mb-5  bg-red-900 rounded-xl p-10 py-8'>
             <NavBar></NavBar>
 
             <p className='text-white text-center text-5xl font-serif font-bold pt-20 pb-3'>Educational Toys Collection</p>
             <p className='text-slate-200 text-center font-serif'>Transform playtime into valuable <br /> learning experiences with our educational toys, <br /> fostering cognitive development, creativity, and a love for learning!</p>
 
-            <div className='mb-10 w-full mt-4'>
+            <div className='mb-20 w-full mt-4'>
                 <form onSubmit={handleSubmit} action="" className='ms-96'>
                     <input type="text" name='toy' placeholder='toy name' className='px-10 py-3 rounded-xl ms-10 bg-slate-300' />
                     <input type="submit" value="Search" className="btn btn-accent " />
                 </form>
             </div>
 
-            <div className="overflow-x-auto">
+            <div className="overflow-x-auto ">
                 <table className="table w-full py-10">
                     {/* head */}
                     <thead className='py-5'>
@@ -46,7 +46,7 @@ const AllToys = () => {
                             <th>No</th>
                             <th>seller Name</th>
                             <th>Toy Name</th>
-                            <th>Subcategory</th>
+                            <th>Sub category</th>
                             <th>Price</th>
                             <th>Available Quantity</th>
                             <th>See Details</th>
